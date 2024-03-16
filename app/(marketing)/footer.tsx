673b3c2d85bba82d895ec1edc78edf172f468cf7
Tomas Svojanovsky <tomas.svojanovsky33@gmail.com>
2024-03-16 08:01:42 +0100

    Update Dashboard

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,7 +1,61 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 export function Footer() {
     return (
-        <div className="hidden lg:block h-20 w-full border-t-2 border-slate-200 pt-2">
-            Footer
-        </div>
+        <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
+            <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image
+                        src="/hr.svg"
+                        alt="Croatian"
+                        height={32}
+                        width={40}
+                        className="mr-4 rounded-md"
+                    />
+                    Croation
+                </Button>
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image
+                        src="/es.svg"
+                        alt="Croatian"
+                        height={32}
+                        width={40}
+                        className="mr-4 rounded-md"
+                    />
+                    Spanish
+                </Button>
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image
+                        src="/fr.svg"
+                        alt="Croatian"
+                        height={32}
+                        width={40}
+                        className="mr-4 rounded-md"
+                    />
+                    French
+                </Button>
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image
+                        src="/jp.svg"
+                        alt="Croatian"
+                        height={32}
+                        width={40}
+                        className="mr-4 rounded-md"
+                    />
+                    Japanese
+                </Button>
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image
+                        src="/it.svg"
+                        alt="Croatian"
+                        height={32}
+                        width={40}
+                        className="mr-4 rounded-md"
+                    />
+                    Italian
+                </Button>
+            </div>
+        </footer>
     );
 }
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,4 +1,8 @@
 import Image from "next/image";
+import Link from "next/link";
+import { ClerkLoaded, ClerkLoading, SignedIn, SignedOut, SignInButton, SignUpButton } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 export default function MarketingPage() {
     return (
@@ -7,6 +11,45 @@ export default function MarketingPage() {
         ">
             <div className="relative w-[240px] h-[240px] lg:w-[424px] lg:h-[424px] mb-8 lg:mb-0">
                 <Image src="/hero.svg" fill alt="Hero"/>
+            </div>
+            <div className="flex flex-col items-center gap-y-8">
+                <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
+                    Learn, practice, and master new languages with Lingo.
+                </h1>
+
+                <div className="flex flex-col items-center gap-y-3 max-w-[330px] w-full">
+                    <ClerkLoading>
+                        <Loader className="w-5 h-5 text-muted-foreground animate-spin"/>
+                    </ClerkLoading>
+                    <ClerkLoaded>
+                        <SignedOut>
+                            <SignUpButton
+                                mode="modal"
+                                afterSignInUrl="/learn"
+                                afterSignUpUrl="/learn"
+                            >
+                                <Button size="lg" variant="secondary" className="w-full">
+                                    Get Started
+                                </Button>
+                            </SignUpButton>
+
+                            <SignInButton
+                                mode="modal"
+                                afterSignInUrl="/learn"
+                                afterSignUpUrl="/learn"
+                            >
+                                <Button size="lg" variant="primaryOutline" className="w-full">
+                                    I have already have an account
+                                </Button>
+                            </SignInButton>
+                        </SignedOut>
+                        <SignedIn>
+                            <Button size="lg" variant="secondary" className="w-full" asChild>
+                                <Link href="/learn">Continue Learning</Link>
+                            </Button>
+                        </SignedIn>
+                    </ClerkLoaded>
+                </div>
             </div>
         </div>
     );
