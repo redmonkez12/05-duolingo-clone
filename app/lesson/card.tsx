a495fbab06736a8c9b9c936c2eb009add974bb21
Tomas Svojanovsky <tomas.svojanovsky33@gmail.com>
2024-03-19 13:13:05 +0100

    Update progress

--- app/lesson/card.tsx
+++ app/lesson/card.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 import { challenges } from "@/db/schema";
 import { cn } from "@/lib/utils";
+import { useAudio, useKey } from "react-use";
+import { useCallback } from "react";
 
 type Props = {
     id: number;
@@ -28,10 +30,20 @@ export function Card({
                          disabled,
                          type,
                      }: Props) {
+    const [audio, _, controls] = useAudio({ src: audioSrc || "" });
+
+    const handleClick = useCallback(() => {
+        if (disabled) return;
+
+        controls.play();
+        onClick();
+    }, [disabled, onClick, controls]);
+
+    useKey(shortcut, handleClick, {}, [handleClick]);
+
     return (
         <div
-            onClick={() => {
-            }}
+            onClick={handleClick}
             className={cn(
                 "h-full border-2 rounded-xl border-b-4 hover:bg-black/5 p-4 lg:p-6 cursor-pointer active:border-b-2",
                 selected && "border-sky-300 bg-sky-100 hover:bg-sky-100",
@@ -43,6 +55,7 @@ export function Card({
                 type === "ASSIST" && "lg:p-3 w-full"
             )}
         >
+            {audio}
             {imageSrc && (
                 <div
                     className="relative aspect-square mb-4 max-h-[80px] lg:max-h-[150px] w-full"
